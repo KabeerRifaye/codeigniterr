041d728d76f1668d95d906d034be518c4d743327
Kabeer Rifaye <kabeer.r@seocompany.com>
2015-07-23 20:05:18 +0530

    fix

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -53,7 +53,7 @@ $autoload['packages'] = array();
 */
 
 // $autoload['libraries'] = array();
-$autoload['libraries'] = array('database','session','form_validation');
+$autoload['libraries'] = array('session','form_validation');
 
 
 /*
