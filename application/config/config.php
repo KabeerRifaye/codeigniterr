e1227c5d514e57072e00708dd1aa70b2eb261d50
Kabeer Rifaye <kabeer.r@seocompany.com>
2015-07-24 10:20:53 +0530

    config file

--- application/config/config.php
+++ application/config/config.php
@@ -16,7 +16,9 @@
 | environments.
 |
 */
-$config['base_url'] = '';
+// $config['base_url'] = '';
+$config['base_url'] = "http".((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "s" : "")."://".$_SERVER['HTTP_HOST'].str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+
 
 /*
 |--------------------------------------------------------------------------
